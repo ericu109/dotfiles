1ecf5188e426e09c4464cbf1e3406ae31e3d2fa3
Eric Urbina <eric@urbina.dev>
2024-10-16 11:03:19 -0600

    update nvim lsp angularls to use language server from current project to more easily work on angular projects of multiple versions

--- .config/nvim/lua/lsp/angular.lua
+++ .config/nvim/lua/lsp/angular.lua
@@ -1,6 +1,28 @@
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+local git_dir = vim.fs.root(vim.loop.cwd(), '.git');
 
-require('lspconfig').angularls.setup({
-  capabilities = capabilities
-})
+if git_dir ~= nil then
+  local project_path = vim.fs.dirname(vim.fs.find({'angular.json'})[1])
+  local node_modules_path = vim.fs.joinpath(project_path, 'node_modules')
+  local ngServerPath = vim.fs.joinpath(node_modules_path, '@angular', 'language-server', 'bin', 'ngserver')
+
+  local ngls_cmd = {
+    ngServerPath,
+    '--stdio',
+    '--tsProbeLocations',
+    node_modules_path,
+    '--ngProbeLocations',
+    node_modules_path
+  }
+
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+  require('lspconfig').angularls.setup({
+    cmd = ngls_cmd,
+    on_new_config = function(new_config)
+      new_config.cmd = ngls_cmd
+    end,
+    on_attach = on_attach,
+    capabilities = capabilities
+  })
+end
