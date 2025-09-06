a0894ae9e0cf067b52e7c491199276244140839b
Eric Urbina <eric@urbina.dev>
2025-09-06 17:38:26 -0600

    Add monitorlayout startup script.

---
+++ .scripts/monitorlayout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-1 --primary --mode 2560x1440 --pos 2560x1440 --rotate normal --rate 144.00 --output DP-2 --mode 2560x1440 --pos 0x1440 --rotate normal --output DP-3 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-1 --mode 2560x1440 --pos 2560x0 --rotate normal --output HDMI-1-2 --off --output DP-1-4 --off --output DP-1-5 --off
