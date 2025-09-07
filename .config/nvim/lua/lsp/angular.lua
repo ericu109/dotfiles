ded6f7dd1003e3685f7f22fc5182f14e6e465741
Eric Urbina <eric@urbina.dev>
2025-09-06 23:53:02 -0600

    Stop angular ls from trying to be configured in non-angular folders.

--- .config/nvim/lua/lsp/angular.lua
+++ .config/nvim/lua/lsp/angular.lua
@@ -2,27 +2,30 @@ local git_dir = vim.fs.root(vim.loop.cwd(), '.git');
 
 if git_dir ~= nil then
   local project_path = vim.fs.dirname(vim.fs.find({'angular.json'}, {path = git_dir})[1])
-  local node_modules_path = vim.fs.joinpath(project_path, 'node_modules')
-  local ngServerPath = vim.fs.joinpath(node_modules_path, '@angular', 'language-server', 'bin', 'ngserver')
 
-  local ngls_cmd = {
-    ngServerPath,
-    '--stdio',
-    '--tsProbeLocations',
-    node_modules_path,
-    '--ngProbeLocations',
-    node_modules_path
-  }
+  if project_path ~= nul then
+    local node_modules_path = vim.fs.joinpath(project_path, 'node_modules')
+    local ngServerPath = vim.fs.joinpath(node_modules_path, '@angular', 'language-server', 'bin', 'ngserver')
 
-  local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+    local ngls_cmd = {
+      ngServerPath,
+      '--stdio',
+      '--tsProbeLocations',
+      node_modules_path,
+      '--ngProbeLocations',
+      node_modules_path
+    }
 
-  require('lspconfig').angularls.setup({
-    cmd = ngls_cmd,
-    on_new_config = function(new_config)
-      new_config.cmd = ngls_cmd
-    end,
-    on_attach = on_attach,
-    capabilities = capabilities
-  })
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+    require('lspconfig').angularls.setup({
+      cmd = ngls_cmd,
+      on_new_config = function(new_config)
+        new_config.cmd = ngls_cmd
+      end,
+      on_attach = on_attach,
+      capabilities = capabilities
+    })
+  end
 end
