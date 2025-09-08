61f6d204c5377412f2b7bcba91c2fbb126043572
Eric Urbina <eric@urbina.dev>
2025-09-08 01:08:47 -0600

    move more auto commands to the autocommands.lua file.

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -25,3 +25,61 @@ vim.cmd([[
 
   autocmd BufWritePre * :call TrimWhitespace()
 ]])
+
+-- Close any netrw buffers when entering a new buffers
+-- Mostly this is to close netrw after selecting a file
+vim.api.nvim_create_autocmd({'BufEnter'}, {
+  callback = function(ev)
+    for _, buf_id in pairs(vim.api.nvim_list_bufs()) do
+      if vim.bo[buf_id].filetype == 'netrw' then
+        vim.api.nvim_buf_delete(buf_id, {})
+      end
+    end
+  end
+})
+
+-- Auto format on save
+vim.api.nvim_create_autocmd({'BufWritePre'}, {
+  pattern = {'*'},
+  callback = function()
+    format()
+    vim.cmd.write()
+  end
+})
+
+-- function to switch between formatting with prettier and formatting with the lsp
+function format()
+  local prettierFilesTypes = {
+    "css",
+    "graphql",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "htmlangular",
+    "json",
+    "less",
+    "markdown",
+    "scss",
+    "typescript",
+    "typescriptreact",
+    "yaml",
+  }
+
+  if has_value(prettierFilesTypes, vim.bo.filetype) then
+    -- print('Formatted with Prettier')
+    vim.cmd(':Prettier')
+  else
+    vim.lsp.buf.format({async=false})
+    -- print('Formatted with vim.lsp.buf.format')
+  end
+end
+
+function has_value (tab, val)
+  for index, value in ipairs(tab) do
+    if value == val then
+      return true
+    end
+  end
+
+  return false
+end
