971a19e7105c8a6bb08e807f4606dba91b92e242
Eric Urbina <eric@urbina.dev>
2026-07-02 21:28:49 -0600

    neovim: auto reformat keymaps.lua

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,7 +1,5 @@
 -- remap space to nop, as it will be used as the leader key
-vim.api.nvim_set_keymap('n', '<Space>', '<NOP>', {noremap = true, silent = true})
-
--- set the leader key to space
+vim.api.nvim_set_keymap('n', '<Space>', '<NOP>', { noremap = true, silent = true }) -- set the leader key to space
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
@@ -10,65 +8,74 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- ch in normal mode to clear highlighting after searching
-vim.api.nvim_set_keymap('n', 'ch', ':nohlsearch<CR>:syntax sync fromstart<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', 'ch', ':nohlsearch<CR>:syntax sync fromstart<CR>', { noremap = true, silent = true })
 
 -- Create undo break points on , . ! and ?
-vim.api.nvim_set_keymap('i', ',', ',<c-g>u', {noremap = true})
-vim.api.nvim_set_keymap('i', '.', '.<c-g>u', {noremap = true})
-vim.api.nvim_set_keymap('i', '!', '!<c-g>u', {noremap = true})
-vim.api.nvim_set_keymap('i', '?', '?<c-g>u', {noremap = true})
+vim.api.nvim_set_keymap('i', ',', ',<c-g>u', { noremap = true })
+vim.api.nvim_set_keymap('i', '.', '.<c-g>u', { noremap = true })
+vim.api.nvim_set_keymap('i', '!', '!<c-g>u', { noremap = true })
+vim.api.nvim_set_keymap('i', '?', '?<c-g>u', { noremap = true })
 
 -- shortcuts for moving between windows
-vim.api.nvim_set_keymap('n', '<leader>h', ':wincmd h<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>j', ':wincmd j<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>k', ':wincmd k<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>l', ':wincmd l<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>h', ':wincmd h<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>j', ':wincmd j<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>k', ':wincmd k<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>l', ':wincmd l<CR>', { noremap = true, silent = true })
 
 -- open netrw as a left banner
-vim.api.nvim_set_keymap('n', '<leader>pv', ':wincmd v<bar> :Ex <bar> :vertical resize 30<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>pv', ':wincmd v<bar> :Ex <bar> :vertical resize 30<CR>', { noremap = true })
 
 -- navigation / finding files
-vim.api.nvim_set_keymap('n', '<leader>pf', '<cmd>lua require("telescope.builtin").git_files({layout_strategy="vertical",show_untracked=true,layout_config={width=0.99}})<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>ps', '<cmd>lua require("telescope.builtin").live_grep({layout_strategy="vertical",layout_config={width=0.99}})<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>pf',
+  '<cmd>lua require("telescope.builtin").git_files({layout_strategy="vertical",show_untracked=true,layout_config={width=0.99}})<CR>',
+  { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ps',
+  '<cmd>lua require("telescope.builtin").live_grep({layout_strategy="vertical",layout_config={width=0.99}})<CR>',
+  { noremap = true, silent = true })
 
 -- vertical resize (width resize) for a window
-vim.api.nvim_set_keymap('n', '<leader>+', ':vertical resize +10<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>-', ':vertical resize -10<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>+', ':vertical resize +10<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>-', ':vertical resize -10<CR>', { noremap = true })
 
 -- remap ctrl-backspace to delete a word
-vim.api.nvim_set_keymap('i', '<C-BS>', '<C-w>', {noremap = true})
-vim.api.nvim_set_keymap('i', '<C-h>', '<C-w>', {noremap = true})
+vim.api.nvim_set_keymap('i', '<C-BS>', '<C-w>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<C-h>', '<C-w>', { noremap = true })
 
 -- ctrl-a in normal mode to go to visual mode and select all
 vim.api.nvim_set_keymap('n', '<C-a>', '<esc>ggVG', {})
 
 -- set a shortcut for opening the undo tree
-vim.api.nvim_set_keymap('n', '<leader>u', ':UndotreeToggle<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>u', ':UndotreeToggle<CR>', { noremap = true })
 
 -- LSP/completion/formatting Keymaps
-vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', 'gr', '<cmd>lua require("telescope.builtin").lsp_references({layout_strategy="vertical",layout_config={width=0.99},fname_width=100})<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', 'gs', '<cmd>lua require("telescope.builtin").treesitter({layout_strategy="vertical",layout_config={width=0.99}})<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>q', '<cmd>lua vim.lsp.buf.code_action()<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>fs', '<cmd>lua require("telescope.builtin").spell_suggest()<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>rr', '<cmd>lua vim.lsp.buf.rename()<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>f', '<cmd>lua format()<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'gr',
+  '<cmd>lua require("telescope.builtin").lsp_references({layout_strategy="vertical",layout_config={width=0.99},fname_width=100})<CR>',
+  { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'gs',
+  '<cmd>lua require("telescope.builtin").treesitter({layout_strategy="vertical",layout_config={width=0.99}})<CR>',
+  { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>q', '<cmd>lua vim.lsp.buf.code_action()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fs', '<cmd>lua require("telescope.builtin").spell_suggest()<CR>',
+  { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>rr', '<cmd>lua vim.lsp.buf.rename()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>f', '<cmd>lua format()<CR>', { noremap = true, silent = true })
 
 -- < or > in visual mode to move the indentation level of selected lines
-vim.api.nvim_set_keymap('v', '<', '<gv', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('v', '>', '>gv', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '<', '<gv', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true, silent = true })
 
 -- Move selected line / block of text in visual mode
-vim.api.nvim_set_keymap('x', 'K', ':move \'<-2<CR>gv-gv', {noremap=true, silent=true})
-vim.api.nvim_set_keymap('x', 'J', ':move \'<+1<CR>gv-gv', {noremap=true, silent=true})
+vim.api.nvim_set_keymap('x', 'K', ':move \'<-2<CR>gv-gv', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('x', 'J', ':move \'<+1<CR>gv-gv', { noremap = true, silent = true })
 
 -- Hold on to what's in the register when pasting over something, instead of yanking what was pasted over
-vim.api.nvim_set_keymap('v', 'p', '"_dP', {noremap=true, silent=true})
+vim.api.nvim_set_keymap('v', 'p', '"_dP', { noremap = true, silent = true })
 
 -- signature help, and viewing diagnostic info
-vim.api.nvim_set_keymap('i', '<c-k>', '<cmd>:lua vim.lsp.buf.signature_help({focusable=false})<CR>', {noremap=true})
-vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>:lua vim.diagnostic.open_float()<CR>', {noremap=true})
+vim.api.nvim_set_keymap('i', '<c-k>', '<cmd>:lua vim.lsp.buf.signature_help({focusable=false})<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>:lua vim.diagnostic.open_float()<CR>', { noremap = true })
 
 -- diagnostic navigation
 vim.keymap.set('n', '<leader>nd', function() vim.diagnostic.goto_next() end)
